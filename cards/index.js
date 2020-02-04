0edcbb93c70f4adf51b1a5ca93b8936ebc3363ce
Bipul Yadav <krbipul.90@gmail.com>
2020-02-05 02:43:54 +0530

    Edit DOne

--- cards/index.js
+++ cards/index.js
@@ -6,16 +6,28 @@ class Input extends React.Component{
 
     }
     handleInputChange = (event)=>{
-            console.log(event.target.value);
+           // console.log(event.target.value);
             this.setState({
                 name:event.target.value.toUpperCase()
             })
     }
     handleSpecialitiesChange = (event)=>{
-        console.log(event.target.value);
+       // console.log(event.target.value);
         this.setState({
             specialities:event.target.value
         })
+    }
+    componentDidMount=()=>{
+        console.log("Mount>>>>>>>>>>",this.props)
+    }
+    componentDidUpdate=(prevProps)=>{
+        console.log("Update>>>>>>>>>>",this.props,prevProps)
+        if(prevProps.editData!==this.props.editData){
+                this.setState({
+                    name:this.props.editData.name,
+                    specialities:this.props.editData.specialities
+                })
+        }
     }
 
     render(){
@@ -29,7 +41,8 @@ class Input extends React.Component{
                     <option>Network Specialists</option>
                     <option>QA Testers</option>
                 </select>
-                <button onClick={()=>{this.props.sendFunctiontoInput(this.state);this.setState({name:""})}} >ADD</button>
+                {this.props.isEdit && <button onClick={()=>{this.props.sendFunctiontoInput(this.state);this.setState({name:""})}} >Update</button>}
+                {!this.props.isEdit &&  <button onClick={()=>{this.props.sendFunctiontoInput(this.state);this.setState({name:""})}} >ADD</button>}
                     
             </div>
         )
@@ -47,7 +60,7 @@ class List extends React.Component{
                         <li className="card">
                             <h2>{data.name}</h2><h3>{data.specialities}</h3>
                             <div className="buttons">
-                                <button onClick={()=>alert("Bhai kaam Chal Rha h Is button par")} >Edit</button>
+                                <button onClick={()=>this.props.sendFunctionToReceiveIndex(index)} >Edit</button>
                                 <button onClick={()=>this.props.sendFunctionToDelete(index)}>Delete</button>
                             </div>
                         </li>
@@ -64,12 +77,24 @@ class List extends React.Component{
 
 class Wrapper extends React.Component{
     state = {
-        card:[]
+        card:[],
+        isEdit:false,
+        editData:"",
+        editIndex:""
+
     }
 
     sendDatatoList=(data)=>{
-            console.log(data);
-            if(data.name){
+           if(this.state.isEdit){
+               let editedData = this.state.card
+               editedData[this.state.editIndex]=data
+               console.log("Edited Data>>>>>>>",editedData)
+               this.setState({
+                   card:editedData,
+                   isEdit:false
+               })
+           }
+            else if(!this.state.isEdit && data.name.trim().length>0){
             this.setState({
                 card:[...this.state.card,data]
             })
@@ -81,11 +106,20 @@ class Wrapper extends React.Component{
             card:this.state.card
         })
     }
+    indexReceivingFun=(index)=>{
+        this.setState({
+            isEdit:true,
+            editData:this.state.card[index],
+            editIndex:index
+
+        })
+        console.log(index);
+    }
     render(){
         return(
             <div className="container">
-                <Input sendFunctiontoInput={(data)=>{this.sendDatatoList(data)}}/>
-                <List sendFunctionToDelete={(index)=>{this.deleteDataFromCard(index)}} sendDataToRender = {this.state.card}/>
+                <Input isEdit={this.state.isEdit} editData={this.state.editData} sendFunctiontoInput={(data)=>{this.sendDatatoList(data)}}/>
+                <List sendFunctionToReceiveIndex={(index)=>{this.indexReceivingFun(index)}} sendFunctionToDelete={(index)=>{this.deleteDataFromCard(index)}} sendDataToRender = {this.state.card}/>
             </div>
 
         )
